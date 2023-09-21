9164bc52a43427cac9d7c0020348bb6dabf0405a
NomadHuns <kimalss22@gmail.com>
2023-09-21 23:56:30 +0900

    구글 맵 및 검색창 구현

---
+++ lib/constants/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData theme = ThemeData(
+  appBarTheme: _appBarTheme
+);
+
+AppBarTheme _appBarTheme = AppBarTheme(
+  color: Color(0x00E4E6ED),
+);
---
+++ lib/pages/main/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:welcome_to_k/pages/main/components/main_google_map.dart';
+import 'package:welcome_to_k/pages/main/components/main_search_bar.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome2K'),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          MainGoogleMap(),
+          MainSearchBar(),
+        ],
+      ),
+    );
+  }
+}
