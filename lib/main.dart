af6d0b811af6091778902f7bfadfc7ef0f0fc00c
NomadHuns <kimalss22@gmail.com>
2023-09-23 22:07:25 +0900

    메인 페이지 리모델링

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:welcome_to_k/constants/theme.dart';
-import 'package:welcome_to_k/pages/route_list/route_list_page.dart';
+import 'package:welcome_to_k/pages/main/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const RouteListPage(),
+      home: const MainPage(),
       theme: theme,
     );
   }
--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:welcome_to_k/pages/main/components/main_carousel.dart';
 import 'package:welcome_to_k/pages/main/components/main_google_map.dart';
 import 'package:welcome_to_k/pages/main/components/main_search_bar.dart';
@@ -9,9 +10,9 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome2K', style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold)),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Welcome2K', style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold)),
+      // ),
       body: Column(
         children: [
           const Expanded(
@@ -27,6 +28,14 @@ class MainPage extends StatelessWidget {
             viewportFraction: 0.8,
             borderRadius: 8.0,
           ),
+        ],
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: 0,
+        destinations: [
+          NavigationDestination(icon: Icon(FontAwesomeIcons.house, color: Colors.blueAccent), label: 'Home'),
+          NavigationDestination(icon: Icon(FontAwesomeIcons.road, color: Colors.blueAccent), label: 'Route'),
+          NavigationDestination(icon: Icon(FontAwesomeIcons.film, color: Colors.blueAccent), label: 'Category'),
         ],
       ),
     );
